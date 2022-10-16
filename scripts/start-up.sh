76ea58709ad908b93c08f5029919f05b585e5e91
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-10-17 00:58:05 +0530

    Updated Docker Files for pre-built react app

--- scripts/start-up.sh
+++ scripts/start-up.sh
@@ -1,8 +1,14 @@
 # ENV VAR $WDB_URL
 BASEDIR=$(dirname "$0")
 
-CONFIG_PATH=$1/src/assets
-sh $(dirname "$0")/gen-wdb-secret.sh $WDB_URL $CONFIG_PATH
+OUT_FILE=$1/src/assets/wdb.secrets.json
 
-cat $CONFIG_PATH/wdb.secrets.json
+TEMPLATE_CONFIG='{"WDB_URL" : ""}'
+
+echo $TEMPLATE_CONFIG | jq --arg URL "${WDB_URL}"  '.WDB_URL=$URL' > $OUT_FILE
+
+# CONFIG_PATH=$1/src/assets
+# sh $(dirname "$0")/gen-wdb-secret.sh $WDB_URL $CONFIG_PATH
+
+cat $OUT_FILE
 npm run build
